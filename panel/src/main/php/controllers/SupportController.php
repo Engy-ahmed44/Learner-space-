a23abc09c5fdab4910e23d3be4afad5a52e144a5
EtshD1 <mohamed.h.abouelenin@gmail.com>
2024-01-06 17:41:04 +0200

    Remaining Controllers

---
+++ panel/src/main/php/controllers/SupportController.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace panel\controllers;
+
+
+use panel\config\Controller;
+use panel\repositories\pdo\MySqlPDODatabase;
+use panel\domain\Admin;
+use panel\auth\AuthManager;
+use panel\dao\SupportTopicDAO;
+
+
+/**
+ * Responsible for the behavior of the SupportView.
+ */
+class SupportController extends Controller
+{
+    //-----------------------------------------------------------------------
+    //        Constructor
+    //-----------------------------------------------------------------------
+    /**
+     * Checks whether admin is logged in and if he has authorization to access 
+     * the page. If he is not, redirects him to login page.
+     */
+    public function __construct()
+    {
+        if (!AuthManager::isLogged() || !$this->hasLoggedAdminAuthorization(0, 2)) {
+            $this->redirectTo("login");
+        }
+    }
+
+
+    //-----------------------------------------------------------------------
+    //        Methods
+    //-----------------------------------------------------------------------
+    /**
+     * @Override
+     */
+    public function index()
+    {
+        $dbConnection = MySqlPDODatabase::shared();
+        $admin = AuthManager::getLoggedIn($dbConnection);
+        $supportDao = new SupportTopicDAO($dbConnection, $admin);
+        $index = $this->getIndex();
+        $limit = 10;
+        $offset = $limit * ($index - 1);
+        $header = array(
+            'title' => 'Support - Learning platform',
+            'styles' => array('SupportStyle', 'searchBar'),
+            'robots' => 'index'
+        );
+        $viewArgs = array(
+            'header' => $header,
+            'username' => $admin->getName(),
+            'authorization' => $admin->getAuthorization(),
+            'topics' => $supportDao->getAllOpened($limit, $offset),
+            'categories' => $supportDao->getCategories(),
+            'scripts' => array('SupportScript'),
+            'totalPages' => ceil($supportDao->count() / $limit),
+            'currentIndex' => $index
+        );
+
+        $this->loadTemplate("support/SupportView", $viewArgs);
+    }
+
+    private function getIndex()
+    {
+        if (!$this->hasIndexBeenSent()) {
+            return 1;
+        }
+
+        return ((int) $_GET['index']);
+    }
+
+    private function hasIndexBeenSent()
+    {
+        return  !empty($_GET['index']);
+    }
+
+    /**
+     * Opens a support topic to read.
+     */
+    public function open($idTopic)
+    {
+        $dbConnection = MySqlPDODatabase::shared();
+        $admin = AuthManager::getLoggedIn($dbConnection);
+        $supportTopicDao = new SupportTopicDAO($dbConnection, $admin);
+        $topic = $supportTopicDao->get((int) $idTopic);
+
+        if (empty($topic)) {
+            $this->redirectTo("support");
+        }
+
+        if ($this->hasReplyBeenSent()) {
+            $this->insertReply($idTopic);
+            $this->reload();
+        }
+
+        $header = array(
+            'title' => 'Support - Learning platform',
+            'styles' => array('SupportStyle', 'message'),
+            'description' => "Support topic",
+            'robots' => 'noindex'
+        );
+        $viewArgs = array(
+            'header' => $header,
+            'username' => $admin->getName(),
+            'authorization' => $admin->getAuthorization(),
+            'topic' => $topic->setDatabase($dbConnection),
+        );
+
+        $this->loadTemplate("support/SupportContentView", $viewArgs);
+    }
+
+    private function hasReplyBeenSent()
+    {
+        return  !empty($_POST['topic_message']);
+    }
+
+    private function insertReply($idTopic)
+    {
+        $dbConnection = MySqlPDODatabase::shared();
+        $admin = AuthManager::getLoggedIn($dbConnection);
+        $supportTopicDao = new SupportTopicDAO($dbConnection, $admin);
+
+        $supportTopicDao->newReply(
+            $idTopic,
+            $_POST['topic_message']
+        );
+
+        unset($_POST['topic_message']);
+    }
+
+    /**
+     * Opens a support topic.
+     */
+    public function unlock($idTopic)
+    {
+        $dbConnection = MySqlPDODatabase::shared();
+        $admin = AuthManager::getLoggedIn($dbConnection);
+        $supportTopicDao = new SupportTopicDAO($dbConnection, $admin);
+        $topic = $supportTopicDao->get($idTopic);
+
+        if (!empty($topic)) {
+            $supportTopicDao->open($idTopic);
+        }
+
+        $this->redirectTo("support");
+    }
+
+    /**
+     * Closes a support topic.
+     */
+    public function lock($idTopic)
+    {
+        $dbConnection = MySqlPDODatabase::shared();
+        $admin = AuthManager::getLoggedIn($dbConnection);
+        $supportTopicDAO = new SupportTopicDAO($dbConnection, $admin);
+        $topic = $supportTopicDAO->get($idTopic);
+
+        if (!empty($topic)) {
+            $supportTopicDAO->close($idTopic);
+        }
+
+        $this->redirectTo("support");
+    }
+
+
+    //-------------------------------------------------------------------------
+    //        Ajax
+    //-------------------------------------------------------------------------
+    /**
+     * Searches support topics that a student has.
+     *
+     * @param		string $_POST['name'] Topic title
+     * @param		string $_POST['filter']['id_category'] Topic category id
+     *
+     * @return      string Support topics
+     */
+    public function search()
+    {
+        if ($this->getHttpRequestMethod() != "POST") {
+            return;
+        }
+
+        $dbConnection = MySqlPDODatabase::shared();
+
+        $supportTopicDao = new SupportTopicDAO(
+            $dbConnection,
+            AuthManager::getLoggedIn($dbConnection)
+        );
+
+        echo json_encode($supportTopicDao->search(
+            $_POST['name'],
+            (int) $_POST['filter']['id_category']
+        ));
+    }
+}
